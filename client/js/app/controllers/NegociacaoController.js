5ad8617b7a75e44fb7379522870d2d9930d9144d
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-02-27 22:46:08 -0300

    Configurando a criação de um objeto Date

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,15 +1,17 @@
 class NegociacaoController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade")
+        this._inputValor = $("#valor")
+    }
+    
     adiciona(event) {
         event.preventDefault()
-        
-        let $ = document.querySelector.bind(document);
-        let inputData = $("#data")
-        let inputQuantidade = $("#quantidade")
-        let inputValor = $("#valor")
-        
-        console.log(inputData.value())
-        console.log(inputQuantidade.value())
-        console.log(inputValor.value())
+    
+        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        console.log(data)
     }
 }
