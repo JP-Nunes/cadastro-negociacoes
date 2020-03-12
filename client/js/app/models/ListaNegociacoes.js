a9f26e50d6c9b5deb2e9e97fb45d4c1da2114de2
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-11 21:46:06 -0300

    Implementação do funcionamento do botão apagar

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -9,6 +9,10 @@ class ListaNegociacoes {
     }
 
     get negociacoes() {
-        return [].concat(this._negociacoes);
+        return [].concat(this._negociacoes)
+    }
+
+    esvazia() {
+        this._negociacoes = []
     }
 }
